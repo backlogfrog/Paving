73a39ef4d0e979355f6fa0f44b50827ff39a461f
backlogfrog <72526671+backlogfrog@users.noreply.github.com>
2021-07-26 09:37:20 -0500

    Update mapping.py
    Updated comments

--- mapping.py
+++ mapping.py
@@ -1,27 +1,31 @@
 #inspection data locations based on column number
 #############################################################
-##########       COLUMNS START AT 0 MOTHERFUCK ################
-# subtract 1 from actual excel column number or your data is fucked
+##########         COLUMNS START AT 0        ################
+# subtract 1 from actual excel column number or your data
 #############################################################
+
 
 #Size REQUIRED for accurate PCI#
 INSPECTED_SIZE=3
-#no set date at this time
+
 INSPECTED_DATE=41
 #Name which will be truncated ex: CS Smith Rd. to CSSSmithRd in scratchXML
 INSPECTED_PID1=2
 INSPECTED_PID2=0
+#comment if any
 DCOMMENT=57
+#width and length of road
 P_LENGTH=3
 P_WIDTH=4
 
-
-SAMPLENUMBER=3
-SAMPLESIZE=40
 #distresscode data locations based on column
 #E= exctent, S= severity
 #Severity: 1= low, 2=med, 3=high
 #Need quantity and other distresses
+
+SAMPLENUMBER=3
+SAMPLESIZE=40
+
 
 SWEATHERING_CODE=4
 SWEATHERING_S=5
